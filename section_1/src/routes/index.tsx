708774ac3c34e5ead070f2a83254e6e6cd70798d
Danilo Lima <97111518+danilolima17@users.noreply.github.com>
2023-03-30 17:43:26 -0300

    Create index.tsx

---
+++ section_1/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes as Switch, Route } from 'react-router-dom';
+import { Dashboard } from '../pages/Dashboard';
+import { Repo } from '../pages/Repo/index';
+
+export const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route element={<Dashboard />} path="/" />
+      <Route element={<Repo />} path="/repositories" />
+    </Switch>
+  );
+};
